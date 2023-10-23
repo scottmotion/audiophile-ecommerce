44cbd73ac5cc2bb56b060a44343e40f016670c49
scottmotion <scott@scottmotion.com>
2023-10-23 09:09:24 -0700

    Update tailwind.config.js
    typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -15,7 +16,58 @@ export default {
         "white": "#ffffff",
         "black": "#000000",
       },
+      fontSize: {
+        base: "0.9375rem",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        body: { fontSize: theme("fontSize.base"), lineHeight: "25px" },
+        h1: {
+          fontSize: "56px",
+          lineHeight: "58px",
+          letterSpacing: "2px",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        },
+        h2: {
+          fontSize: "40px",
+          lineHeight: "44px",
+          letterSpacing: "1.5px",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        },
+        h3: {
+          fontSize: "32px",
+          lineHeight: "36px",
+          letterSpacing: "1.15px",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        },
+        h4: {
+          fontSize: "28px",
+          lineHeight: "38px",
+          letterSpacing: "2px",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        },
+        h5: {
+          fontSize: "24px",
+          lineHeight: "33px",
+          letterSpacing: "1.7px",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        },
+        h6: {
+          fontSize: "18px",
+          lineHeight: "24px",
+          letterSpacing: "1.3px",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        },
+      });
+    }),
+  ],
 };
