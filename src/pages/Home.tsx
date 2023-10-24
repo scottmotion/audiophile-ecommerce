ee20f356ea36f0f913fda3ad2a4abe8bfb3c58d6
scottmotion <scott@scottmotion.com>
2023-10-24 14:51:47 -0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,16 @@
 export default function Home() {
   return (
     <>
+      <nav className="flex flex-row justify-between gap-3 bg-black text-white">
+        <img src="/src/assets/logo.svg" />
+        <ul className="flex flex-row gap-3">
+          <li>Headphones</li>
+          <li>Speakers</li>
+          <li>Earphones</li>
+        </ul>
+        <div>Cart</div>
+      </nav>
+
       <h1>Home</h1>
       <div className="flex flex-row gap-3">
         <button className="btn btn-1">Button 1</button>
