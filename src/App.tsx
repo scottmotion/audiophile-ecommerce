8e473a55a18f0e2ed02e748b224103cdf88f4d59
scottmotion <scott@scottmotion.com>
2023-10-24 14:26:03 -0700

    Revert "navbar"
    This reverts commit 356c7bded855a7094ee03cd1d8db71f4ec98fbee.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 function App() {
   return (
     <>
-      <Navbar />
       <Home />
+      {/* <h1>Vite + React</h1>
+      <div className="flex flex-row gap-3">
+        <button className="btn btn-1">Button 1</button>
+        <button className="btn btn-2">Button 2</button>
+        <button className="btn btn-3">Button 3</button>
+        <button className="btn btn-4">Button 4</button>
+      </div>
+      <p>This is a Paragraph</p> */}
     </>
   );
 }
