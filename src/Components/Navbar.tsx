c5805757ca03139c8a4589a6b1b5c11a8e6f1ae3
scottmotion <scott@scottmotion.com>
2023-10-24 16:38:57 -0700

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,15 +1,22 @@
 import { ReactComponent as SiteLogo } from "/src/assets/logo.svg";
+import { ReactComponent as CartIcon } from "/src/assets/icons/icon-cart.svg";
+import { ReactComponent as MobileMenuIcon } from "/src/assets/icons/icon-hamburger.svg";
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row justify-between gap-3 bg-black text-white">
-      <SiteLogo />
-      <ul className="flex flex-row gap-3">
+    <nav className="flex flex-row items-center justify-between gap-3 bg-black text-white">
+      <div className="flex flex-row items-center justify-center gap-3">
+        <MobileMenuIcon className="lg:hidden" />
+        <SiteLogo className="hidden md:block" />
+      </div>
+      <SiteLogo className="md:hidden" />
+
+      <ul className="hidden flex-row items-center justify-center gap-3 lg:flex">
         <li>Headphones</li>
         <li>Speakers</li>
         <li>Earphones</li>
       </ul>
-      <div>Cart</div>
+      <CartIcon />
     </nav>
   );
 }
