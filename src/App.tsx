331f694b6749c02d276176f1ba2b7787070a41f9
scottmotion <scott@scottmotion.com>
2023-10-24 08:39:04 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div>
         <button
           onClick={() => setCount(count => count + 1)}
-          className="btn btn-4"
+          className="btn btn-1"
         >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="text-base">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
