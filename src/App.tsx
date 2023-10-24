7837c51e6e56f442b208e0cec1b93290478e8f44
scottmotion <scott@scottmotion.com>
2023-10-24 14:45:14 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,6 @@ function App() {
   return (
     <>
       <Home />
-      {/* <h1>Vite + React</h1>
-      <div className="flex flex-row gap-3">
-        <button className="btn btn-1">Button 1</button>
-        <button className="btn btn-2">Button 2</button>
-        <button className="btn btn-3">Button 3</button>
-        <button className="btn btn-4">Button 4</button>
-      </div>
-      <p>This is a Paragraph</p> */}
     </>
   );
 }
