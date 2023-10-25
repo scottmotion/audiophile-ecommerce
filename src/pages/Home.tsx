b1e5f18ce03a6a3b5e0df24f98bbb489976d86ca
scottmotion <scott@scottmotion.com>
2023-10-25 11:38:07 -0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,22 +12,54 @@ export default function Home() {
         <p>This is a Paragraph</p>
       </div> */}
       <div className="flex flex-col">
-        <div className="relative bg-black">
+        <section className="flex flex-col">
+          <div className="relative bg-black">
+            <img
+              src="src/assets/home/mobile/image-header.jpg"
+              alt="Page header"
+              className="content-home-mobile md:content-home-tablet lg:content-home-desktop w-full"
+            ></img>
+          </div>
+          <div className="absolute">
+            <p className="text-overline text-white opacity-50">New Product</p>
+            <h2 className="text-white">XX99 Mark II HeadphoneS</h2>
+            <p className="text-white opacity-75">
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+            <button className="btn btn-1">See Product</button>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <p>Headphones</p>
+            <button className="btn btn-4">Shop</button>
+          </div>
+          <div>
+            <p>Speakers</p>
+            <button className="btn btn-4">Shop</button>
+          </div>
+          <div>
+            <p>Earphones</p>
+            <button className="btn btn-4">Shop</button>
+          </div>
+        </section>
+
+        <section className="m-6 flex flex-col items-center gap-8 rounded-lg bg-dark-orange px-8 py-14">
           <img
-            src="/src/assets/home/mobile/image-header.jpg"
-            alt="Page header"
-            className="content-home-mobile md:content-home-tablet lg:content-home-desktop w-full"
+            className="w-2/3"
+            src="src/assets/home/mobile/image-speaker-zx9.png"
           ></img>
-        </div>
-        <div className="absolute">
-          <p className="text-overline text-white opacity-50">New Product</p>
-          <h2 className="text-white">XX99 Mark II HeadphoneS</h2>
-          <p className="text-white opacity-75">
-            Experience natural, lifelike audio and exceptional build quality
-            made for the passionate music enthusiast.
-          </p>
-          <button className="btn btn-1">See Product</button>
-        </div>
+          <div className="flex flex-col items-center gap-6 text-center">
+            <p>ZX9 SPEAKER</p>
+            <p>
+              Upgrade to premium speakers that are phenomenally built to deliver
+              truly remarkable sound.
+            </p>
+            <button className="btn btn-2">See Product</button>
+          </div>
+        </section>
       </div>
     </>
   );
