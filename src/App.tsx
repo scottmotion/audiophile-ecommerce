f5be3c165f2c4fa17f3f0b754324597b7905c7a1
scottmotion <scott@scottmotion.com>
2023-10-26 09:31:47 -0700

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import Navbar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout";
+
 import Home from "./pages/Home";
-import Footer from "./Components/Footer";
+import Headphones from "./pages/Headphones";
+import Speakers from "./pages/Speakers";
+import Earphones from "./pages/Earphones";
+
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter basename="/audiophile-ecommerce/">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="headphones" element={<Headphones />} />
+            <Route path="speakers" element={<Speakers />} />
+            <Route path="earphones" element={<Earphones />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
