b56e1a8853b16168cb69a47e73dadbfecbba2d37
scottmotion <scott@scottmotion.com>
2023-10-26 11:17:52 -0700

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 flex flex-row items-center justify-between gap-3 border-b border-white/[.10] bg-black px-6 py-8 text-white">
-      <div className="flex flex-row items-center gap-3">
+      <div className="flex shrink basis-1/5 flex-row items-center gap-3">
         <MobileMenuIcon className="lg:hidden" />
         <SiteLogo className="hidden md:block" />
       </div>
-      <SiteLogo className="md:hidden" />
+      <SiteLogo className="overflow-visible md:hidden" />
 
-      <ul className="hidden flex-row items-center gap-3 lg:flex">
+      <ul className="hidden shrink-0 basis-3/4 flex-row items-center justify-center gap-3 lg:flex">
         <li className="nav-item">
           <Link to="/">Home</Link>
         </li>
@@ -26,7 +26,9 @@ export default function Navbar() {
           <Link to="/earphones">Earphones</Link>
         </li>
       </ul>
-      <CartIcon />
+      <div className="flex shrink basis-1/5 flex-row justify-end">
+        <CartIcon />
+      </div>
     </nav>
   );
 }
