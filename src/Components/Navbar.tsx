c1e2aff537ae5964508391432c6499d64b52dd95
scottmotion <scott@scottmotion.com>
2023-10-26 13:53:17 -0700

    Update Navbar.tsx
    transparent on home page, black on rest

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,16 @@
 import { ReactComponent as SiteLogo } from "/src/assets/logo.svg";
 import { ReactComponent as CartIcon } from "/src/assets/icons/icon-cart.svg";
 import { ReactComponent as MobileMenuIcon } from "/src/assets/icons/icon-hamburger.svg";
-import { Link } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 
 export default function Navbar() {
+  const match = useMatch("/");
+
+  const backgroundColor = match ? "" : "bg-black";
   return (
-    <nav className="sticky top-0 z-50 flex flex-row items-center justify-between gap-3 border-b border-white/[.10] bg-black px-6 py-8 text-white">
+    <nav
+      className={`${backgroundColor} sticky top-0 z-50 flex flex-row items-center justify-between gap-3 border-b border-white/[.10] px-6 py-8 text-white`}
+    >
       <div className="flex shrink basis-1/5 flex-row items-center gap-3">
         <MobileMenuIcon className="lg:hidden" />
         <SiteLogo className="hidden md:block" />
