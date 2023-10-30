1fae916467fe08a86b6ac50222f8e7ef48640a48
scottmotion <scott@scottmotion.com>
2023-10-30 15:54:50 -0700

    Update FeaturedProducts.tsx

--- src/Components/FeaturedProducts.tsx
+++ src/Components/FeaturedProducts.tsx
@@ -1,21 +1,6 @@
 export default function FeaturedProducts() {
   return (
     <section className="flex w-full max-w-[1100px] flex-col items-center gap-8">
-      {/* <div className="flex w-full flex-col items-center gap-8 rounded-lg bg-dark-orange px-8 py-14">
-        <img
-          className="w-2/3"
-          src="src/assets/home/mobile/image-speaker-zx9.png"
-        ></img>
-        <div className="flex flex-col items-center gap-6 text-center">
-          <p>ZX9 SPEAKER</p>
-          <p>
-            Upgrade to premium speakers that are phenomenally built to deliver
-            truly remarkable sound.
-          </p>
-          <button className="btn btn-2">See Product</button>
-        </div>
-      </div> */}
-
       <div className="grid w-full grid-cols-1 gap-x-3 gap-y-6 md:grid-cols-2">
         <div className="col-span-1 flex w-full flex-col items-center gap-8 rounded-lg bg-dark-orange px-8 py-14 md:col-span-2">
           <img
@@ -33,10 +18,25 @@ export default function FeaturedProducts() {
         </div>
 
         <div className="col-span-1 flex flex-col justify-center rounded-lg bg-medium-grey md:col-span-2">
-          <img
+          <picture>
+            <source
+              media="(min-width: 1024px)"
+              srcSet="src/assets/home/desktop/image-speaker-zx7.jpg"
+            />
+            <source
+              media="(min-width: 768px)"
+              srcSet="src/assets/home/tablet/image-speaker-zx7.jpg"
+            />
+            <img
+              src="src/assets/home/mobile/image-speaker-zx7.jpg"
+              className="w-full rounded-lg"
+            />
+          </picture>
+
+          {/* <img
             src="src/assets/home/mobile/image-speaker-zx7.jpg"
             className="w-full rounded-lg"
-          ></img>
+          ></img> */}
           <div className="absolute p-6">
             <p>ZX7 Speaker</p>
             <button className="btn btn-3">See Product</button>
