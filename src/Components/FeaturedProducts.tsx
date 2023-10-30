f307a76317812eb0ece921e9259c00d0428a6f4e
scottmotion <scott@scottmotion.com>
2023-10-30 15:29:25 -0700

    Update FeaturedProducts.tsx

--- src/Components/FeaturedProducts.tsx
+++ src/Components/FeaturedProducts.tsx
@@ -23,7 +23,7 @@ export default function FeaturedProducts() {
             src="src/assets/home/mobile/image-speaker-zx9.png"
           ></img>
           <div className="flex flex-col items-center gap-6 text-center">
-            <p>ZX9 SPEAKER</p>
+            <p>ZX9 Speaker</p>
             <p>
               Upgrade to premium speakers that are phenomenally built to deliver
               truly remarkable sound.
@@ -31,20 +31,29 @@ export default function FeaturedProducts() {
             <button className="btn btn-2">See Product</button>
           </div>
         </div>
-        <div className="col-span-1 rounded-lg bg-medium-grey md:col-span-2">
+
+        <div className="col-span-1 flex flex-col justify-center rounded-lg bg-medium-grey md:col-span-2">
           <img
             src="src/assets/home/mobile/image-speaker-zx7.jpg"
             className="w-full rounded-lg"
           ></img>
+          <div className="absolute p-6">
+            <p>ZX7 Speaker</p>
+            <button className="btn btn-3">See Product</button>
+          </div>
         </div>
-        <div className="col-span-1 rounded-lg bg-dark-grey">
-          <img
-            src="src/assets/home/mobile/image-earphones-yx1.jpg"
-            className="w-full rounded-lg"
-          ></img>
-        </div>
-        <div className="col-span-1 rounded-lg bg-light-grey">
-          <p>YX1 EARPHONES</p>
+
+        <div className="col-span-1 grid grid-cols-1 grid-rows-2 gap-x-3 gap-y-6 md:col-span-2 md:grid-cols-2 md:grid-rows-1">
+          <div className="col-span-1 rounded-lg bg-dark-grey">
+            <img
+              src="src/assets/home/mobile/image-earphones-yx1.jpg"
+              className="w-full rounded-lg"
+            ></img>
+          </div>
+          <div className="col-span-1 flex flex-col items-start justify-center rounded-lg bg-light-grey p-6">
+            <p>YX1 Earphones</p>
+            <button className="btn btn-3">See Product</button>
+          </div>
         </div>
       </div>
     </section>
