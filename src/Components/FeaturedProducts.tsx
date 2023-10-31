6483f7f36bf4755dbfb32b88b23cfef17620ffc7
scottmotion <scott@scottmotion.com>
2023-10-30 19:07:45 -0700

    Update FeaturedProducts.tsx

--- src/Components/FeaturedProducts.tsx
+++ src/Components/FeaturedProducts.tsx
@@ -2,6 +2,38 @@ export default function FeaturedProducts() {
   return (
     <section className="flex w-full max-w-[1100px] flex-col items-center gap-8">
       <div className="grid w-full grid-cols-1 gap-x-3 gap-y-6 md:grid-cols-2">
+        <div className="grid grid-cols-1 grid-rows-2 rounded-lg bg-dark-orange">
+          <div className="flex w-full flex-col">
+            <img
+              src="src/assets/home/desktop/pattern-circles.svg"
+              className="absolute"
+            ></img>
+
+            <picture className="flex justify-center">
+              <source
+                media="(min-width: 1024px)"
+                srcSet="src/assets/home/desktop/image-speaker-zx9.png"
+              />
+              <source
+                media="(min-width: 768px)"
+                srcSet="src/assets/home/tablet/image-speaker-zx9.png"
+              />
+              <img
+                src="src/assets/home/mobile/image-speaker-zx9.png"
+                className="w-2/3"
+              />
+            </picture>
+          </div>
+
+          <div className="flex flex-col items-center gap-6 text-center">
+            <p>ZX9 Speaker</p>
+            <p>
+              Upgrade to premium speakers that are phenomenally built to deliver
+              truly remarkable sound.
+            </p>
+            <button className="btn btn-2">See Product</button>
+          </div>
+        </div>
         <div className="col-span-1 flex w-full flex-col items-center gap-8 rounded-lg bg-dark-orange px-8 py-14 md:col-span-2">
           <picture className="flex justify-center">
             <source
@@ -17,11 +49,6 @@ export default function FeaturedProducts() {
               className="w-2/3"
             />
           </picture>
-          {/* 
-          <img
-            className="w-2/3"
-            src="src/assets/home/mobile/image-speaker-zx9.png"
-          ></img> */}
           <div className="flex flex-col items-center gap-6 text-center">
             <p>ZX9 Speaker</p>
             <p>
