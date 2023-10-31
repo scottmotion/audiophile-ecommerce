bf0eb5c3b1be19f6ecdc2fa9206ed90b35fa378e
scottmotion <scott@scottmotion.com>
2023-10-31 10:59:08 -0700

    Update Headphones.tsx

--- src/pages/Headphones.tsx
+++ src/pages/Headphones.tsx
@@ -3,13 +3,13 @@ import BestGear from "../Components/BestGear";
 export default function Headphones() {
   return (
     <>
-      <header className="flex w-full flex-col items-center bg-black">
-        <div className="flex w-full flex-col items-center justify-center bg-black">
-          <h1 className="text-white">Headphones</h1>
+      <header className="mb-[4rem] flex w-full flex-col items-center bg-black md:mb-[7.5rem]">
+        <div className="flex w-full flex-col items-center justify-center bg-black py-8 md:py-24">
+          <h1 className="text-3xl text-white">Headphones</h1>
         </div>
       </header>
 
-      <main className="mb-[7.5rem] flex w-full flex-col items-center gap-[7.5rem] px-6 md:gap-24 md:px-10 lg:gap-[12.5rem]">
+      <main className="mb-[7.5rem] flex w-full flex-col items-center gap-[7.5rem] px-6 md:px-10 lg:mb-[12.5rem] lg:gap-[10rem]">
         <CategoryNav />
         <BestGear />
       </main>
