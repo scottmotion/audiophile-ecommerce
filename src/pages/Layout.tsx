ff0b2d7f2a895e92848da7a4eed702d664ce3625
scottmotion <scott@scottmotion.com>
2023-11-01 10:03:44 -0700

    scroll restoration
    scroll restoration

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import { ScrollRestoration } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
+      <ScrollRestoration />
       <Navbar />
       <Outlet />
       <Footer />
