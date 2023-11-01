e405d55b3968506bbc26d5735a81baa45aa43026
scottmotion <scott@scottmotion.com>
2023-11-01 13:56:38 -0700

    Update CategoryLayout.tsx

--- src/pages/CategoryLayout.tsx
+++ src/pages/CategoryLayout.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { fetchData, fetchCategory } from "../api/fetchApi";
+import { fetchCategory } from "../api/fetchApi";
 import { ProductData } from "../types/ProductType";
 
 import CategoryNav from "../Components/CategoryNav";
@@ -10,11 +10,10 @@ type CategoryLayoutProps = {
   category: string;
 };
 
-// const allData: ProductData[] = await fetchData();
-
 export default function CategoryLayout({ category }: CategoryLayoutProps) {
   const [products, setProducts] = useState<ProductData[] | null>(null);
 
+  // Get category product data
   useEffect(() => {
     async function startFetching() {
       setProducts(null);
@@ -33,22 +32,11 @@ export default function CategoryLayout({ category }: CategoryLayoutProps) {
 
   console.log("products: ", products);
 
-  // console.log("All Data: ", allData);
-
-  // let categoryData: {}[] = [];
-  // allData.forEach((element: ProductData) => {
-  //   if (element.category == category) {
-  //     categoryData.push(element);
-  //   }
-  // });
-
-  // console.log("categoryData: ", categoryData);
-
   return (
     <>
       <header className="mb-[4rem] flex w-full flex-col items-center bg-black md:mb-[7.5rem]">
         <div className="flex w-full flex-col items-center justify-center bg-black py-8 md:py-24">
-          <h1 className="text-3xl text-white md:text-4xl">C: {category}</h1>
+          <h1 className="text-3xl text-white md:text-4xl">{category}</h1>
         </div>
       </header>
 
