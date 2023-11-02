4acd75695191cb6f72110e19c5f010616f4a9e07
scottmotion <scott@scottmotion.com>
2023-11-02 10:13:04 -0700

    Update ProductLayout.tsx

--- src/pages/ProductLayout.tsx
+++ src/pages/ProductLayout.tsx
@@ -69,6 +69,7 @@ export default function ProductLayout() {
                 <img src={currentProduct.image.mobile} className="rounded-lg" />
               </picture>
               <div className="col-span-1 flex flex-col items-center gap-6 text-center lg:col-start-1 lg:row-start-1 lg:items-start lg:text-left">
+                {currentProduct.new && <p>New Product</p>}
                 <p>{currentProduct.name}</p>
                 <p>{currentProduct.description}</p>
                 <button className="btn btn-1">Add to Cart</button>
