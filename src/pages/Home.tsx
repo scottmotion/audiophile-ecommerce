f8c0c3af4a45a0cd7e620a427d439b68b2283e75
scottmotion <scott@scottmotion.com>
2023-11-01 17:11:03 -0700

    Update Home.tsx
    remove z-index class on header to expose child element button as clickable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ import BestGear from "../Components/BestGear";
 export default function Home() {
   return (
     <>
-      <header className="relative -z-10 -mt-[90px] mb-10 flex w-full flex-col items-center bg-black md:mb-[6rem] lg:mb-[7.5rem]">
+      <header className="relative -mt-[90px] mb-10 flex w-full flex-col items-center bg-black md:mb-[6rem] lg:mb-[7.5rem]">
         <div className="w-full bg-black">
           <img
             src="src/assets/home/mobile/image-header.jpg"
