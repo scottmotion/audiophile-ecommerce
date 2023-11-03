5f802c6f7de8d2296ac34a726cdf424f2bf2c7f5
scottmotion <scott@scottmotion.com>
2023-11-03 15:43:34 -0700

    Update Checkout.tsx

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 export default function Checkout() {
   return (
-    <div>
-      <p>Checkout</p>
-    </div>
+    <>
+      <nav className="flex w-full flex-col items-center justify-center px-6 md:px-10 lg:bg-light-grey">
+        <div className="flex w-full max-w-[1100px] flex-row pb-6 pt-4 md:pt-8 lg:pb-14 lg:pt-20">
+          <Link to={".."} relative="path">
+            <button>
+              <p>Go Back</p>
+            </button>
+          </Link>
+        </div>
+      </nav>
+      <main className="flex w-full flex-col items-center gap-[7.5rem] px-6 md:px-10 lg:gap-[10rem] lg:bg-light-grey">
+        <section className="mb-[7.5rem] flex w-full max-w-[1100px] flex-col items-center gap-20 rounded-lg bg-white lg:mb-[12.5rem]">
+          <p>Checkout</p>
+        </section>
+      </main>
+    </>
   );
 }
