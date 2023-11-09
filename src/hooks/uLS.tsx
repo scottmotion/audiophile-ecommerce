687bdbf7a59d5259baa4e7cd198d931d2d1d50c4
scottmotion <scott@scottmotion.com>
2023-11-08 16:43:05 -0800

    Update uLS.tsx

--- src/hooks/uLS.tsx
+++ src/hooks/uLS.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+export function uLS<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
     if (jsonValue != null) return JSON.parse(jsonValue);
