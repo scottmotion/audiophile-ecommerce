76e80a3cc04eb2801db4e89d89907a04928dc912
scottmotion <scott@scottmotion.com>
2023-11-12 16:01:11 -0800

    Update Checkout.tsx

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -11,21 +11,25 @@ export default function Checkout() {
           </Link>
         </div>
       </nav>
-      <main className="flex w-full flex-col items-center gap-[2rem] bg-light-grey px-6 pb-24 md:px-10 md:pb-[7.25rem] lg:pb-[8.75rem]">
+      <main className="flex w-full flex-col gap-[2rem] bg-light-grey px-6 pb-24 md:px-10 md:pb-[7.25rem] lg:pb-[8.75rem]">
         <section className="flex w-full max-w-content flex-col items-center rounded-lg bg-white">
           <h1 className="heading-5 md:heading-4">Checkout</h1>
           <div>
-            <h2>Billing Details</h2>
+            <h2 className="text-subtitle">Billing Details</h2>
           </div>
           <div>
-            <h2>Shipping Info</h2>
+            <h2 className="text-subtitle">Shipping Info</h2>
           </div>
           <div>
-            <h2>Payment Details</h2>
+            <h2 className="text-subtitle">Payment Details</h2>
           </div>
         </section>
-        <section className="flex w-full max-w-content flex-col items-center rounded-lg bg-white">
+        <section className="flex w-full max-w-content flex-col rounded-lg bg-white px-6 py-8">
           <h2 className="heading-7">Summary</h2>
+          <p>Total</p>
+          <p>Shipping</p>
+          <p>VAT (included)</p>
+          <p>Grand Total</p>
           <button className="btn btn-1 w-full"> Continue & Pay</button>
         </section>
       </main>
