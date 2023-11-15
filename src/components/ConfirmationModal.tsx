12d9599fe1ef253a52406193ef898b1002b8c012
scottmotion <scott@scottmotion.com>
2023-11-15 07:32:53 -0800

    Update ConfirmationModal.tsx

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import { ReactComponent as ConfirmationIcon } from "/src/assets/icons/icon-order-confirmation.svg";
+
 type ConfirmationModalProps = {
   setShowConfirmation: React.Dispatch<React.SetStateAction<boolean>>;
 };
@@ -15,9 +17,27 @@ export default function ConfirmationModal({
         onClick={() => setShowConfirmation(false)}
       ></div>
       <div
-        id="confirmation"
-        className="absolute left-1/2 top-1/2 z-40 flex max-h-[calc(100vh-8rem)] w-full max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 flex-col justify-start gap-3 overflow-y-auto rounded-lg bg-white p-6 md:max-w-[23.5rem]"
+        id="confirmationModal"
+        className="absolute left-1/2 top-1/2 z-40 flex max-h-[calc(100vh-8rem)] w-full max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 flex-col justify-start gap-3 overflow-y-auto rounded-lg bg-white p-8 md:max-w-[23.5rem]"
       >
+        <ConfirmationIcon />
+        <h2 className="heading-6 leading-[28px] tracking-[0.86px] md:heading-4">
+          Thank you
+          <br />
+          for your order
+        </h2>
+        <p>You will receive an email confirmation shortly.</p>
+        <div className="flex flex-col overflow-hidden rounded-lg">
+          <div className="flex flex-col items-center gap-3 bg-light-grey p-6">
+            <div>1st Item</div>
+            <hr className="w-full text-medium-grey" />
+            <div className="label opacity-50">and 2 other item(s)</div>
+          </div>
+          <div className="flex flex-col gap-2 bg-black p-6 text-white">
+            <p className="uppercase">Grand Total</p>
+            <p className="heading-7 opacity-100">$ 5432</p>
+          </div>
+        </div>
         <Link
           to={"/"}
           relative="path"
@@ -26,7 +46,7 @@ export default function ConfirmationModal({
           <button className="btn btn-1 w-full">Back to Home</button>
         </Link>
         <button
-          className="btn btn-2"
+          className="btn btn-3"
           onClick={() => setShowConfirmation(false)}
         >
           Close Modal
