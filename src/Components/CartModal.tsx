4ead58a9567d6f02398684e83c69fae79db6b197
scottmotion <scott@scottmotion.com>
2023-11-17 12:21:06 -0800

    Update CartModal.tsx

--- src/Components/CartModal.tsx
+++ src/Components/CartModal.tsx
@@ -25,7 +25,7 @@ export default function CartModal({ setShowCart }: CartModalProps) {
       ></div>
       <div
         id="cart"
-        className="absolute right-0 top-20 z-40 flex max-h-[calc(100vh-8rem)] w-full max-w-[calc(100vw-3rem)] flex-col justify-start gap-3 overflow-y-auto rounded-lg bg-white p-6 md:max-w-[23.5rem]"
+        className="absolute right-0 top-20 z-40 flex max-h-[calc(100vh-8rem)] w-full max-w-[calc(100vw-3rem)] flex-col justify-start gap-8 overflow-y-auto rounded-lg bg-white px-7 py-8 md:max-w-[23.5rem]"
       >
         {/* Cart Header */}
         <div className="flex flex-row items-center justify-between">
@@ -40,65 +40,68 @@ export default function CartModal({ setShowCart }: CartModalProps) {
         </div>
 
         {/* Cart Items */}
-        {cartItems.map(item => (
-          <div
-            className="flex flex-row items-center justify-between gap-4"
-            key={item.id}
-          >
-            <div className="flex flex-row gap-4">
-              <img
-                src={`/assets/cart/image-${item.slug}.jpg`}
-                className="w-16 rounded-lg"
-              />
-              <div className="flex flex-col justify-center">
-                <p className="font-bold uppercase opacity-100">
-                  {item.shortName}
+        <div className="flex flex-col gap-6">
+          {cartItems.map(item => (
+            <div
+              className="flex flex-row items-center justify-between gap-4"
+              key={item.id}
+            >
+              <div className="flex flex-row gap-4">
+                <img
+                  src={`/assets/cart/image-${item.slug}.jpg`}
+                  className="w-16 rounded-lg"
+                />
+                <div className="flex flex-col justify-center">
+                  <p className="font-bold uppercase opacity-100">
+                    {item.shortName}
+                  </p>
+                  <p className="font-bold">$ {item.price.toLocaleString()}</p>
+                </div>
+              </div>
+
+              <div className="flex flex-row items-center justify-center bg-light-grey">
+                <button
+                  className="btn-quantity-modal opacity-50 hover:text-dark-orange hover:opacity-100"
+                  onClick={() => decreaseItemQuantity(item.id)}
+                >
+                  -
+                </button>
+                <p className="btn-quantity-modal select-none opacity-100">
+                  {cartItems.find(i => i.id === item.id)?.quantity}
                 </p>
-                <p>$ {item.price.toLocaleString()}</p>
+                <button
+                  className="btn-quantity-modal opacity-50 hover:text-dark-orange hover:opacity-100"
+                  onClick={() => increaseItemQuantity(item.id)}
+                >
+                  +
+                </button>
               </div>
             </div>
-
-            <div className="flex flex-row items-center justify-center bg-light-grey">
-              <button
-                className="btn-quantity opacity-50 hover:text-dark-orange hover:opacity-100"
-                onClick={() => decreaseItemQuantity(item.id)}
-              >
-                -
-              </button>
-              <p className="btn-quantity select-none opacity-100">
-                {cartItems.find(i => i.id === item.id)?.quantity}
-              </p>
-              <button
-                className="btn-quantity opacity-50 hover:text-dark-orange hover:opacity-100"
-                onClick={() => increaseItemQuantity(item.id)}
-              >
-                +
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
 
         {/* Cart Footer */}
-        {cartQuantity > 0 ? (
-          <div className="flex flex-row items-center justify-between">
-            <p>Total</p>
-            <p className="heading-7 opacity-100">
-              $ {cartTotal.toLocaleString()}
-            </p>
-          </div>
-        ) : (
-          <p className="flex justify-center">Your cart is empty</p>
-        )}
-
-        {cartQuantity > 0 ? (
-          <Link to={"/checkout"} onClick={() => setShowCart(false)}>
-            <button className="btn btn-1 w-full">Checkout</button>
-          </Link>
-        ) : (
-          <button className="btn btn-1" onClick={() => setShowCart(false)}>
-            Continue Shopping
-          </button>
-        )}
+        <div className="flex flex-col gap-6">
+          {cartQuantity > 0 ? (
+            <div className="flex flex-row items-center justify-between">
+              <p>TOTAL</p>
+              <p className="heading-7 opacity-100">
+                $ {cartTotal.toLocaleString()}
+              </p>
+            </div>
+          ) : (
+            <p className="flex justify-center">Your cart is empty</p>
+          )}
+          {cartQuantity > 0 ? (
+            <Link to={"/checkout"} onClick={() => setShowCart(false)}>
+              <button className="btn btn-1 w-full">Checkout</button>
+            </Link>
+          ) : (
+            <button className="btn btn-1" onClick={() => setShowCart(false)}>
+              Continue Shopping
+            </button>
+          )}
+        </div>
       </div>
     </>
   );
