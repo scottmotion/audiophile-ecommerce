3caa08f564ec86350345ecc642a70e52f0d3270e
scottmotion <scott@scottmotion.com>
2023-11-21 14:21:16 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/audiophile-ecommerce',
+  base: '/audiophile-ecommerce/',
   plugins: [react(), svgr()],
 })
