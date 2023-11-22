88a12f6aa25834343238c2945919ec0572037ab8
scottmotion <scott@scottmotion.com>
2023-11-21 16:12:41 -0800

    Revert "BASE_URL"
    This reverts commit 0d81871cf7e39acf065262652b52d340e8618c83.

--- src/api/fetchApi.tsx
+++ src/api/fetchApi.tsx
@@ -1,4 +1,4 @@
-const apiUrl = import.meta.env.BASE_URL + "/data/data.json";
+const apiUrl = "/data/data.json";
 import { ProductData } from "../types/ProductType";
 
 export async function fetchData() {
